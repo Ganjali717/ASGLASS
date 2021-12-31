8064403dd149744d27ddba23a76c321a6496f794
Ganjali717 <ganjaliki@code.edu.az>
2021-12-31 17:32:52 +0400

    Admin Panel Profile

--- ASGlass/ASGlass/Areas/Manage/Controllers/AccountController.cs
+++ ASGlass/ASGlass/Areas/Manage/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using ASGlass.Areas.Manage.ViewModels;
+using ASGlass.DAL;
 using ASGlass.Models;
+using ASGlass.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,18 +14,19 @@ namespace ASGlass.Areas.Manage.Controllers
 {
     [Area("manage")]
 
-
     public class AccountController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly AppDbContext _context;
 
-        public AccountController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<AppUser> signInManager)
+        public AccountController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<AppUser> signInManager, AppDbContext context)
         {
             _userManager = userManager;
             _roleManager = roleManager;
             _signInManager = signInManager;
+            _context = context;
         }
         public IActionResult Index()
         {
@@ -150,6 +153,74 @@ namespace ASGlass.Areas.Manage.Controllers
             await _signInManager.SignInAsync(admin, true);
 
             return RedirectToAction("index", "dashboard");
+        }
+
+
+        public async Task<IActionResult> Profile()
+        {
+            AppUser appUser = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            ProfileViewModel passwordVM = new ProfileViewModel
+            {
+                appUser = appUser
+            };
+            return View(passwordVM);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin, SuperAdmin")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Profile(ProfileViewModel profileVM)
+        {
+            TempData["Success"] = false;
+
+
+            AppUser member = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            if (!string.IsNullOrWhiteSpace(profileVM.ConfirmPassword) && !string.IsNullOrWhiteSpace(profileVM.NewPassword))
+            {
+                var passwordChangeResult = await _userManager.ChangePasswordAsync(member, profileVM.OldPassword, profileVM.NewPassword);
+
+                if (!passwordChangeResult.Succeeded)
+                {
+                    foreach (var item in passwordChangeResult.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+
+                    return View();
+                }
+
+            }
+
+            if (member.Email != profileVM.Email && _userManager.Users.Any(x => x.NormalizedEmail == profileVM.Email.ToUpper()))
+            {
+                ModelState.AddModelError("Email", "Bu Email adresi istifadə olunub!");
+                return View();
+            }
+
+            member.FullName = profileVM.FullName;
+            member.Email = profileVM.Email;
+            member.PhoneNumber = profileVM.PhoneNumber;
+            member.UserName = profileVM.UserName;
+
+            var result = await _userManager.UpdateAsync(member);
+
+            await _signInManager.SignInAsync(member, true);
+
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+
+                return View();
+            }
+
+            TempData["Success"] = true;
+
+            return RedirectToAction("showaccount");
         }
     }
 }
